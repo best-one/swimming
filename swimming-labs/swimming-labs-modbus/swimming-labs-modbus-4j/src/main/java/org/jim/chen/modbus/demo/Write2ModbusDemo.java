c7131f2961aaecd1a726f55d4d0c4f08a8d2726c
jim <feilong274@qq.com>
2021-10-18 13:47:56 +0800

    modbus test

--- swimming-labs/swimming-labs-modbus/swimming-labs-modbus-4j/src/main/java/org/jim/chen/modbus/demo/Write2ModbusDemo.java
+++ swimming-labs/swimming-labs-modbus/swimming-labs-modbus-4j/src/main/java/org/jim/chen/modbus/demo/Write2ModbusDemo.java
@@ -19,9 +19,14 @@ public class Write2ModbusDemo {
 
 	public static void main(String[] args) {
 		while (true) {
+//			int a = ra(0,65535);
+//			System.out.println(a);
+//			if(a<0) {System.exit(0);}
+			
 			writeList();
+			//chinaMobileIot();
 			try {
-				TimeUnit.SECONDS.sleep(10);
+				TimeUnit.SECONDS.sleep(1);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
@@ -29,11 +34,42 @@ public class Write2ModbusDemo {
 
 	}
 
+	public static void chinaMobileIot() {
+		write(2, 0, ra(0, 200), DataType.EIGHT_BYTE_INT_SIGNED);
+		write(2, 1, ra(0, 170), DataType.EIGHT_BYTE_INT_SIGNED);
+		write(2, 2, ra(0, 65535), DataType.EIGHT_BYTE_INT_SIGNED);
+		write(2, 3, ra(0, 65535), DataType.EIGHT_BYTE_INT_SIGNED);
+		write(2, 4, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+		
+		write(2, 7, ra(0, 65535), DataType.EIGHT_BYTE_INT_SIGNED);
+		write(2, 8, ra(0, 65535), DataType.EIGHT_BYTE_INT_SIGNED);
+		write(2, 9, ra(0, 65535), DataType.EIGHT_BYTE_INT_SIGNED);
+		
+		write(2, 10, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+		write(2, 11, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+		write(2, 12, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+		write(2, 13, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+		write(2, 14, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+	}
+	
 	public static void writeList() {
 		List<ValueObj> list = new ArrayList<>();
 		
 		list.add(ValueObj.of(2, 0, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED));
 		list.add(ValueObj.of(2, 1, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED));
+		list.add(ValueObj.of(2, 2, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED));
+		list.add(ValueObj.of(2, 3, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED));
+		list.add(ValueObj.of(2, 4, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED));
+		
+		list.add(ValueObj.of(2, 7, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED));
+		list.add(ValueObj.of(2, 8, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED));
+		list.add(ValueObj.of(2, 9, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED));
+		
+		list.add(ValueObj.of(2, 10, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED));
+		list.add(ValueObj.of(2, 11, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED));
+		list.add(ValueObj.of(2, 12, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED));
+		list.add(ValueObj.of(2, 13, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED));
+		list.add(ValueObj.of(2, 14, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED));
 		try {
 			Modbus4jWriteUtils.writeList(list);
 		} catch (ModbusInitException | ModbusTransportException | ErrorResponseException e) {
@@ -42,46 +78,49 @@ public class Write2ModbusDemo {
 	}
 	
 	public static void test1() {
-		write(2, 0, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
-		write(2, 1, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
-		write(2, 2, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
-		write(2, 3, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+		write(2, 0, ra(0, 200), DataType.EIGHT_BYTE_INT_SIGNED);
+		write(2, 1, ra(0, 170), DataType.EIGHT_BYTE_INT_SIGNED);
+		write(2, 2, ra(0, 65535), DataType.EIGHT_BYTE_INT_SIGNED);
+		write(2, 3, ra(0, 65535), DataType.EIGHT_BYTE_INT_SIGNED);
 		write(2, 4, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
-		write(2, 5, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
-		write(2, 6, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
-		write(2, 7, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
-		write(2, 8, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
-		write(2, 9, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+//		write(2, 5, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+//		write(2, 6, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+		
+//		write(2, 7, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+//		write(2, 8, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+//		write(2, 9, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+		
+		
 		write(2, 10, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
 		write(2, 11, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
 		write(2, 12, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
 		write(2, 13, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
 		write(2, 14, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
-		write(2, 15, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
-		write(2, 16, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
-		write(2, 17, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
-		write(2, 18, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
-		write(2, 19, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
-		write(2, 20, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
-		write(2, 21, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
-		write(2, 22, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
-		write(2, 23, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
-		write(2, 24, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
-		write(2, 25, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
-		write(2, 26, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
-		write(2, 27, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
-		write(2, 28, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
-		write(2, 29, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
-		write(2, 30, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
-		write(2, 31, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
-		write(2, 32, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
-		write(2, 33, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
-		write(2, 34, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
-		write(2, 35, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
-		write(2, 36, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
-		write(2, 37, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
-		write(2, 38, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
-		write(2, 39, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+//		write(2, 15, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+//		write(2, 16, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+//		write(2, 17, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+//		write(2, 18, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+//		write(2, 19, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+//		write(2, 20, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+//		write(2, 21, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+//		write(2, 22, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+//		write(2, 23, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+//		write(2, 24, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+//		write(2, 25, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+//		write(2, 26, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+//		write(2, 27, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+//		write(2, 28, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+//		write(2, 29, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+//		write(2, 30, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+//		write(2, 31, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+//		write(2, 32, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+//		write(2, 33, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+//		write(2, 34, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+//		write(2, 35, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+//		write(2, 36, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+//		write(2, 37, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+//		write(2, 38, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
+//		write(2, 39, ra(0, 9999), DataType.EIGHT_BYTE_INT_SIGNED);
 	}
 	
 	public static void test2() {
